a8fcc2e667c6f287ec7205947fbdbf52ce8710e1
dmavrotas <dmavrotas@hotmail.com>
2022-02-13 20:23:30 +0100

    fix: miscellaneous
    * fix typos
    * fix wrong references

--- src/main/java/com/dmavrotas/pts/api/models/interfaces/Identifiable.java
+++ src/main/java/com/dmavrotas/pts/api/models/interfaces/Identifiable.java
@@ -3,7 +3,7 @@ package com.dmavrotas.pts.api.models.interfaces;
 import com.dmavrotas.pts.api.utils.AssignedIdentityGenerator;
 
 /**
- * @GenericGenerator(name = "assigned-identity", strategy = "com.kinaxia.kds.repository.utils.AssignedIdentityGenerator")
+ * @GenericGenerator(name = "assigned-identity", strategy = "com.dmavrotas.pts.api.utils.AssignedIdentityGenerator")
  * @GeneratedValue(generator = "assigned-identity", strategy = GenerationType.IDENTITY)
  * @see AssignedIdentityGenerator
  * <p>
